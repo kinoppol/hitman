484bdc1ea7e3d87a0cebf8d9c8d2e9f4553b6ee1
kinoppol <kinoppol@hotmail.com>
2025-09-20 20:20:29 +0700

    Update tracking.php

---
+++ room/tracking.php
@@ -0,0 +1,16 @@
+<?php
+require_once('../db.php');
+$sql='select count(*) as remain from target where status="live"';
+$result=$sql->query($sql);
+$r=$result->fetch_assoc();
+print 'คงเหลือ '.$r['remain'].' รายชื่อ<br>';
+
+$sql='select count(*) as male_remain from target_data td left join target t on td.id=t.id where status="live" and td.gender="male"';
+$result=$sql->query($sql);
+$r=$result->fetch_assoc();
+print 'เพศชายเหลือ '.$r['male_remain'].' รายชื่อ<br>';
+
+$sql='select count(*) as female_remain from target_data td left join target t on td.id=t.id where status="live" and td.gender="female"';
+$result=$sql->query($sql);
+$r=$result->fetch_assoc();
+print 'เพศหญิงเหลือ '.$r['female_remain'].' รายชื่อ<br>';
