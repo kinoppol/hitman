3570f5f2af241e34c6479a0ce38b05e97c294f11
kinoppol <kinoppol@hotmail.com>
2026-06-07 13:14:26 +0700

    Update tracking.php

--- room/tracking.php
+++ room/tracking.php
@@ -5,12 +5,18 @@ $result=$db->query($sql);
 $r=$result->fetch_assoc();
 print 'คงเหลือ '.$r['remain'].' รายชื่อ<br>';
 
-$sql='select count(*) as male_remain from target_data td left join target t on td.id=t.id where status="live" and td.gender="male"';
+$sql='select * from target_data td left join target t on td.id=t.id where status="live" and td.gender="male"';
 $result=$db->query($sql);
-$r=$result->fetch_assoc();
-print 'เพศชายเหลือ '.$r['male_remain'].' รายชื่อ<br>';
+$num=0;
+while($r=$result->fetch_assoc()){
+$num++;
+}
+print 'เพศชายเหลือ '.$num.' รายชื่อ<br>';
 
-$sql='select count(*) as female_remain from target_data td left join target t on td.id=t.id where status="live" and td.gender="female"';
+$sql='select * from target_data td left join target t on td.id=t.id where status="live" and td.gender="female"';
 $result=$db->query($sql);
-$r=$result->fetch_assoc();
-print 'เพศหญิงเหลือ '.$r['female_remain'].' รายชื่อ<br>';
+$num=0;
+while($r=$result->fetch_assoc()){
+$num++;
+}
+print 'เพศหญิงเหลือ '.$num.' รายชื่อ<br>';
